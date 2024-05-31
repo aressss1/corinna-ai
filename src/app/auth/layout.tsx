b0244231f9ec90740c08916e528f08aa5917bbf4
aressss1 <codewithfaizi@gmail.com>
2024-05-31 10:50:43 +0500

    f-

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,5 @@
 import { currentUser } from "@clerk/nextjs";
+
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -28,6 +29,25 @@ export const Layout =  async ({
                     height={0}
                 />
                 {children}
+            </div>
+            <div className="hidden lg:flex glex-1 w-full max-h-full max-w-4000px overflow-hidden relative bg-cream flex-col pt-10 pl-24 gap-3" >
+                    <h2 className="text-gravel md:text-4xl font-bold" >
+                        Hi, I&apos;m your AI powered sales assistant, Corinna!
+                    </h2>
+                    <p className="text-iridium md:text-sm mb-10" >
+                        Corinna is capable of capturing lead information without a a form...{''}
+                        <br />
+                        something never done before
+                    </p>
+                    <Image 
+                        src="/images/app-ui.png"
+                        alt="logo"
+                        loading="lazy"
+                        sizes="30"
+                        className="absolute shrink-0 !w-[1600px] top-48"
+                        width={0}
+                        height={0}
+                    />
             </div>
         </div>
     );
---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,19 @@
+import { SignUpFormProvider } from "@/components/forms/signup/form-provider";
+import { RegistrationFormStep } from "@/components/forms/signup/registration-step";
+
+type Props = {
+ 
+}
+export const SignUp = ({}: Props) => {
+    return ( 
+        <div className="flex-1 py-36 md:px-16 w-full" >
+            <div className="flex lfex-col h-full gap-3" >
+                <SignUpFormProvider>
+                    <div className="flex flex-col gap-3" >
+                        <RegistrationFormStep></RegistrationFormStep>
+                    </div>
+                </SignUpFormProvider>
+            </div>
+        </div>
+    );
+}
